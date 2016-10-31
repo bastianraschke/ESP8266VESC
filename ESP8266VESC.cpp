5a826dbcd1b8597d6c5d9159560beb0d0cb19cce
Bastian Raschke <bastian.raschke@posteo.de>
2016-10-31 22:32:46 +0100

    Refactoring

--- ESP8266VESC.cpp
+++ ESP8266VESC.cpp
@@ -1,5 +1,6 @@
 #include "ESP8266VESC.h"
 
+// The C files must be included specially for C++
 extern "C"
 {
     #include "crc.h"
@@ -8,15 +9,18 @@ extern "C"
 }
 
 
-ESP8266VESC::ESP8266VESC(SoftwareSerial &serial):
+ESP8266VESC::ESP8266VESC(Stream &serial):
 _serial(serial)
 {
+
 }
 
-void ESP8266VESC::setDuty(float dutyValue)
+void ESP8266VESC::setDutyCycle(float dutyValue)
 {
-    uint8_t payload[5] = {0};
     int32_t index = 0;
+
+    // Length: 1 byte packet ID and 4 bytes for float (32 bit)
+    uint8_t payload[5] = {0};
 
     payload[index++] = COMM_SET_DUTY;
 
@@ -28,8 +32,10 @@ void ESP8266VESC::setDuty(float dutyValue)
 
 void ESP8266VESC::setCurrent(float currentInAmpere)
 {
-    uint8_t payload[5] = {0};
     int32_t index = 0;
+
+    // Length: 1 byte packet ID and 4 bytes for integer (32 bit)
+    uint8_t payload[5] = {0};
 
     payload[index++] = COMM_SET_CURRENT;
 
@@ -41,8 +47,10 @@ void ESP8266VESC::setCurrent(float currentInAmpere)
 
 void ESP8266VESC::setCurrentBrake(float currentInAmpere)
 {
-    uint8_t payload[5] = {0};
     int32_t index = 0;
+
+    // Length: 1 byte packet ID and 4 bytes for integer (32 bit)
+    uint8_t payload[5] = {0};
 
     payload[index++] = COMM_SET_CURRENT_BRAKE;
 
@@ -54,8 +62,10 @@ void ESP8266VESC::setCurrentBrake(float currentInAmpere)
 
 void ESP8266VESC::setRPM(int32_t rpmValue)
 {
-    uint8_t payload[5] = {0};
     int32_t index = 0;
+
+    // Length: 1 byte packet ID and 4 bytes for integer (32 bit)
+    uint8_t payload[5] = {0};
 
     payload[index++] = COMM_SET_RPM;
 
@@ -66,7 +76,14 @@ void ESP8266VESC::setRPM(int32_t rpmValue)
 
 void ESP8266VESC::releaseEngine()
 {
+    // Set no engine current means idling
     setCurrent(0.0f);
+}
+
+void ESP8266VESC::fullBreaking()
+{
+    // Set duty cycle to 0.0 means full breaking
+    setDutyCycle(0.0f);
 }
 
 void ESP8266VESC::_sendPacket(uint8_t* payload, uint16_t length)
--- ESP8266VESC.h
+++ ESP8266VESC.h
@@ -1,9 +1,7 @@
 #ifndef __ESP8266VESC_H__
 #define __ESP8266VESC_H__
 
-#include <SoftwareSerial.h>
-
-
+#include <Stream.h>
 
 #define PACKET_LENGTH_SHORT 0x02;
 #define PACKET_LENGTH_LONG 0x03;
@@ -11,26 +9,53 @@
 #define PACKET_TERMINATION_BYTE 0x03;
 
 
-
 class ESP8266VESC
 {
 public:
-    ESP8266VESC(SoftwareSerial &serial);
 
-    void setDuty(float dutyValue);
+    ESP8266VESC(Stream &serial);
+
+    /**
+     * Set duty cycle value from -1.0 (full throttle backwards) to 1.0 (full throttle forwards).
+     *
+     */
+    void setDutyCycle(float dutyValue);
+
+    /**
+     * Set current value in ampere.
+     *
+     */
     void setCurrent(float currentInAmpere);
+
+    /**
+     * Set current value for breaking in ampere.
+     *
+     */
     void setCurrentBrake(float currentInAmpere);
+
+    /**
+     * Set current RPM value in rounds per minute.
+     *
+     */
     void setRPM(int32_t rpmValue);
+
+    /**
+     * Enable idling of the engine.
+     *
+     */
     void releaseEngine();
 
+    /**
+     * Perform a full breaking of the engine.
+     *
+     */
+    void fullBreaking();
+
 private:
-    SoftwareSerial &_serial;
+
+    Stream &_serial;
 
     void _sendPacket(uint8_t* payload, uint16_t length);
 };
 
 #endif
-
-
-
-
